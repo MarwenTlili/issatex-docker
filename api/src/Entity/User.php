736c0f7dfab8ce8acecbba157484ceb965da38aa
MarwenTlili <marwentlili@gmail.com>
2023-03-04 15:15:04 +0100

    Returning a salt is only needed

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -140,6 +140,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+	/**
+	 * Returning a salt is only needed, if you are not using a modern
+	 * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+	 * @return string|null
+	 */
+	public function getSalt(): ?string {
+		return null;
+	}
+
     /**
      * @see UserInterface
      */
