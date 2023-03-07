d8b2127e7e7a0c646fcdc4808df0ab5f7c765f1e
MarwenTlili <marwentlili@gmail.com>
2023-03-07 14:58:53 +0100

    create tests for JWT auth

---
+++ api/tests/Api/AuthenticationTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Tests;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\User;
+use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
+
+class AuthenticationTest extends ApiTestCase {
+    use ReloadDatabaseTrait;
+
+    public function testLogin(): void {
+        $client = self::createClient();
+        $container = self::getContainer();
+
+        $user = new User();
+		$user->setUsername('test');
+        $user->setEmail('test@example.com');
+        $user->setPassword(
+            $container->get('security.user_password_hasher')->hashPassword($user, '$3CR3T')
+        );
+
+        $manager = $container->get('doctrine')->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        // retrieve a token
+        $response = $client->request('POST', '/auth', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [
+                'username' => 'test',
+                'password' => '$3CR3T',
+            ],
+        ]);
+
+        $json = $response->toArray();
+        $this->assertResponseIsSuccessful();
+        $this->assertArrayHasKey('token', $json);
+
+        // test not authorized
+        $client->request('GET', '/api/users');
+        $this->assertResponseStatusCodeSame(401);
+
+        // test authorized
+        $client->request('GET', '/api/users', ['auth_bearer' => $json['token']]);
+        $this->assertResponseIsSuccessful();
+    }
+}
