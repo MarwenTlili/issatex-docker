ed9fa610856f864a8178c18ce22c2b11a4d5f0ae
MarwenTlili <marwentlili@gmail.com>
2023-03-14 20:57:05 +0100

    whatever

--- pwa/pages/admin/index.tsx
+++ pwa/pages/admin/index.tsx
@@ -2,24 +2,24 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 
 const Admin = () => {
-  // Load the admin client-side
-  const [DynamicAdmin, setDynamicAdmin] = useState(<p>Loading...</p>);
-  useEffect(() => {
-    (async () => {
-      const HydraAdmin = (await import("@api-platform/admin")).HydraAdmin;
+	// Load the admin client-side
+	const [DynamicAdmin, setDynamicAdmin] = useState(<p>Loading...</p>);
+	useEffect(() => {
+		(async () => {
+			const HydraAdmin = (await import("@api-platform/admin")).HydraAdmin;
 
-      setDynamicAdmin(<HydraAdmin entrypoint={window.origin}></HydraAdmin>);
-    })();
-  }, []);
+			setDynamicAdmin(<HydraAdmin entrypoint={window.origin}></HydraAdmin>);
+		})();
+	}, []);
 
-  return (
-    <>
-      <Head>
-        <title>API Platform Admin</title>
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>API Platform Admin</title>
+			</Head>
 
-      {DynamicAdmin}
-    </>
-  );
+			{DynamicAdmin}
+		</>
+	);
 };
 export default Admin;
