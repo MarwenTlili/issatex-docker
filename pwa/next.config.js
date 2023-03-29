48c1431901f7458d478a2a6e778f3ad3ba611668
MarwenTlili <marwentlili@gmail.com>
2023-03-29 22:32:00 +0100

    custom image loader config

--- pwa/next.config.js
+++ pwa/next.config.js
@@ -1,8 +1,18 @@
+/**
+ * https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+ * custom advanced configuration of Next.js.
+ * regular Node.js module.
+ * will not be parsed by Webpack, Babel or TypeScript.
+ * used by the Next.js server and build phases.
+ */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
+	reactStrictMode: true,
+	swcMinify: true,
+	output: 'standalone',
+	images: {
+		domains: ['tailwindui.com', 'flowbite.com', 'via.placeholder.com']
+	},
 }
 
 module.exports = nextConfig
