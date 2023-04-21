4d98699c05b8a732855653625ba0a5fa8cba982e
MarwenTlili <marwentlili@gmail.com>
2023-04-21 18:00:05 +0100

    remove unused props

--- pwa/pages/_app.tsx
+++ pwa/pages/_app.tsx
@@ -10,7 +10,7 @@ type CustomAppProps = AppProps & {
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
-function App( {Component, pageProps: { session, ...pageProps } }: CustomAppProps): JSX.Element {
+function App( {Component, pageProps }: CustomAppProps): JSX.Element {
 	return (
 		<Layout dehydratedState={pageProps.dehydratedState} >
 			<Component {...pageProps} />
