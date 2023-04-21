808e0bfe2784bdfe43424e5687df0f5e1f0ffdb8
MarwenTlili <marwentlili@gmail.com>
2023-04-21 16:06:50 +0100

    allow images from external https sites

--- pwa/next.config.js
+++ pwa/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
+	reactStrictMode: true,
+	swcMinify: true,
+	output: 'standalone',
+	images: {
+		domains: ['tailwindui.com', 'picsum.photos']
+	},
 }
 
 module.exports = nextConfig
