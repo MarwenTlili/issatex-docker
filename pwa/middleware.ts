847349855bfc5de8ad99743c39d5491eb930b124
MarwenTlili <marwentlili@gmail.com>
2023-08-01 21:05:25 +0100

    exclude admin from middleware

--- pwa/middleware.ts
+++ pwa/middleware.ts
@@ -40,11 +40,12 @@ export const config = {
      * Match all request paths except for the ones starting with:
      * - api (API routes)
      * - auth/signin
+     * - admin
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|auth/signin|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|auth/signin|admin|_next/static|_next/image|favicon.ico).*)',
 		// '/articles',
 		// '/articles/(.*)'
 	]
