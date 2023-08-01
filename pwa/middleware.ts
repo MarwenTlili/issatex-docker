8450a2e3acb521b465ef18b0c234067968019bde
MarwenTlili <marwentlili@gmail.com>
2023-08-01 20:33:50 +0100

    exclude admin from middleware

--- pwa/middleware.ts
+++ pwa/middleware.ts
@@ -44,7 +44,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|auth/signin|_next/static|_next/image|favicon.ico).*)',
+    '/((?!admin|api|auth/signin|_next/static|_next/image|favicon.ico).*)',
 		// '/articles',
 		// '/articles/(.*)'
 	]
