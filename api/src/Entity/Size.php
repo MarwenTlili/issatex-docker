585ea4a03ca18ad23559537e2da177bb194884eb
MarwenTlili <marwentlili@gmail.com>
2024-02-05 20:50:25 +0100

    remove useless group

--- api/src/Entity/Size.php
+++ api/src/Entity/Size.php
@@ -8,26 +8,20 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UlidType;
-use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Ulid;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
 
 #[ORM\Entity(repositoryClass: SizeRepository::class)]
-#[ApiResource(
-    normalizationContext: ['groups' => ['size:read']],
-    denormalizationContext: ['groups' => ['size:write']]
-)]
+#[ApiResource]
 #[ApiFilter(SearchFilter::class, properties: ['name' => 'exact'])]
 class Size {
-    #[Groups(['size:read'])]
     #[ORM\Id]
     #[ORM\Column(type: UlidType::NAME, unique: true)]
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(class: 'doctrine.ulid_generator')]
     private ?Ulid $id = null;
 
-    #[Groups(['size:read'])]
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
