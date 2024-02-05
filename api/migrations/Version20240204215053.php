18cb65c6b7a12bf0f3587b51df0bd93424cf29ae
MarwenTlili <marwentlili@gmail.com>
2024-02-05 20:45:47 +0100

    recreate migrations

--- api/migrations/Version20240204215053.php
+++ api/migrations/Version20240204215053.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231206133759 extends AbstractMigration
+final class Version20240204215053 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,13 @@ final class Version20231206133759 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE "user" ALTER email DROP NOT NULL');
+        $this->addSql('ALTER TABLE client ALTER phone DROP NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE "user" ALTER email SET NOT NULL');
+        $this->addSql('ALTER TABLE client ALTER phone SET NOT NULL');
     }
 }
